4962c0825e79791328f498a367cdb0e70ba7ab75
mestefan-cmd <m.estefan@bankaletihad.com>
2026-07-01 10:15:29 +0300

    removed auth from the code and returned back to normal

--- Routes/index.js
+++ Routes/index.js
@@ -1,12 +1,9 @@
 const companyRoutes  = require('./companyRoutes');
 const employeeRoutes = require('./employeeRoutes');
 const categoryRoutes = require('./categoryRoutes');
-const authRoutes     = require('./authRoutes');
-const authenticate   = require('../middleware/auth');
 
 module.exports = (app) => {
-    app.use('/auth',       authRoutes);
-    app.use('/companies',  authenticate, companyRoutes);
-    app.use('/employees',  authenticate, employeeRoutes);
-    app.use('/categories', authenticate, categoryRoutes);
+    app.use('/companies',  companyRoutes);
+    app.use('/employees',  employeeRoutes);
+    app.use('/categories', categoryRoutes);
 };
